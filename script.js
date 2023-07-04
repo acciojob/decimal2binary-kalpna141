744d9a323b52a1374f87bd504703f77c8b564d66
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-07-04 22:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function decimalToBinary(num) {
-  //Write you code here
+// function decimalToBinary(num) {
+//   //Write you code here
   
-}
+// }
 
-window.decimalToBinary = decimalToBinary;
+// window.decimalToBinary = decimalToBinary;
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
+}
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
